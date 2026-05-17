1ed4d6e4c2defcd44b7a00d78caf2639fb357760
TeeHa80 <226384692+TeeHa80@users.noreply.github.com>
2026-05-17 11:28:27 +0200

    Create secrets.example.h

---
+++ esp32/mqtt_lauflicht/secrets.example.h
@@ -0,0 +1,7 @@
+// eigene Werte eintragen und als secrets.h speichern
+
+const char* ssid        = "DeinHotspotName";
+const char* password    = "DeinHotspotPasswort";
+const char* mqtt_server = "10.14.x.x";
+const char* mqtt_user   = "DeinBenutzername";
+const char* mqtt_pw     = "DeinPasswort";
