613673ba7f013180d8f9056163c35bd5dc25ac95
TeeHa80 <226384692+TeeHa80@users.noreply.github.com>
2026-05-17 11:32:18 +0200

    Update secrets.example.h

--- esp32/mqtt_lauflicht/secrets.example.h
+++ esp32/mqtt_lauflicht/secrets.example.h
@@ -1,7 +1,10 @@
 // eigene Werte eintragen und als secrets.h speichern
 
+// WLAN
 const char* ssid        = "DeineSSID";
 const char* password    = "DeinPasswort";
+
+// Mosquitto Broker (Standard-Port: 1883)
 const char* mqtt_server = "x.x.x.x";
 const char* mqtt_user   = "DeinBenutzername";
 const char* mqtt_pw     = "DeinPasswort";
