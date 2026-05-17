ad39dcbf731f8807f758280d18808f12e9141706
TeeHa80 <226384692+TeeHa80@users.noreply.github.com>
2026-05-17 11:28:59 +0200

    Update secrets.example.h

--- esp32/mqtt_lauflicht/secrets.example.h
+++ esp32/mqtt_lauflicht/secrets.example.h
@@ -2,6 +2,6 @@
 
 const char* ssid        = "DeinHotspotName";
 const char* password    = "DeinHotspotPasswort";
-const char* mqtt_server = "10.14.x.x";
+const char* mqtt_server = "x.x.x.x";
 const char* mqtt_user   = "DeinBenutzername";
 const char* mqtt_pw     = "DeinPasswort";
